3303024b249816038ef40ec3e968ab1f782a82d9
Damien <damien.lequeux@gmail.com>
2019-11-20 16:17:30 +0100

    Add Test with word AA

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,10 +1,21 @@
 public class Scrabble {
 
-    public int computeScore(String word){
+    public int computeScore(String word) {
+
         int score = 0;
-        if (word.contains("A")){
-            score = score + 1;
+        for (char letter : word.toCharArray()) {
+            score += getLetterScore(letter);
         }
         return score;
+
+    }
+
+    private int getLetterScore(char letter) {
+        int letterScore = 0;
+        if (letter == 'A') {
+            letterScore = letterScore + 1;
+        }
+
+        return letterScore;
     }
 }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -22,6 +22,6 @@ public class ScrabbleTest {
 
         int score = scrabble.computeScore(word);
 
-        assertThat(score).isEqualTo(1);
+        assertThat(score).isEqualTo(2);
     }
 }
