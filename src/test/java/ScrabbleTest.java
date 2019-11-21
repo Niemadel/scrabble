443ca680a87781891a8ef5c61ac42886a3867756
Damien <damien.lequeux@gmail.com>
2019-11-21 14:37:04 +0100

    refacto test name

--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -95,7 +95,7 @@ public class ScrabbleTest {
     }
 
     @Test
-    void given_string_with_blank_return_score_of_eigtheen() {
+    void given_string_LA_VIE_EST_BELLE_with_blank_return_score_of_eigtheen() {
         Scrabble scrabble = new Scrabble();
         String word = "LA VIE EST BELLE";
 
